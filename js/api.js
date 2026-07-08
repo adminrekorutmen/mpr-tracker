11d85d764c6d400928b9b96932e6d72bb2c408dc
adminrekorutmen <adminrekorutmen@gmail.com>
2026-07-08 16:57:51 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -2,8 +2,8 @@
 // KONEKSI UTAMA KE DATABASE GOOGLE SHEETS
 // ==========================================
 // MASUKKAN URL WEB APP GOOGLE APPS SCRIPT ANDA DI SINI
-const GAS_API_URL = "URL_APLIKASI_WEB_ANDA_DI_SINI"; 
-const API_KEY = "KUNCI_RAHASIA_REKOSISTEM_123";
+const GAS_API_URL = "aHR0cHM6Ly9zY3JpcHQuZ29vZ2xlLmNvbS9tYWNyb3Mvcy9BS2Z5Y2J4bUdMYXhZbk9qd1AtcUNaMV9GMDlHMVFlRGMwblEyejB0ODNCdFlVcG5TMDRMRTZQSGhjVTB0SzVBMnNkczRVWTMvZXhlYw=="; 
+const API_KEY = "S1VOQ0lfUkFIQVNJQV9SRUtPU0lTVEVNXzEyMw==";
 
 // Fungsi pembantu agar pengiriman data ke Google Sheets stabil dan terhindar dari error CORS
 async function callGAS(payload) {
